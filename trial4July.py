f5e1df5b7ce7c631e5ad2b3e502f03f30403476c
gumtekapil <87220319+gumtekapil@users.noreply.github.com>
2021-07-10 11:33:03 +0530

    Add files via upload

---
+++ trial4July.py
@@ -0,0 +1,88 @@
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from scipy.optimize import minimize
+"""
+#single line comment 
+
+This is comment paragraph
+just try it out
+
+x=True
+y=True
+if x==y:
+    print('kapil rules1')
+    
+#int
+x=34
+print(f'x={x}')
+x=3.14**2
+print(f'x={x}')
+x=3+9+4j
+print(x)
+x=complex(5,3)
+print(x)
+print(f'real={x.real},\n imag={x.imag}')
+z='Kapil\'s'
+x=10
+y=25
+if x==y:
+    print('equal to')
+if x>=y:
+    print('greater than')
+if x<=y:
+    print('less than')
+elif x==12:
+    print('x=12')
+else:
+    print('no solution') 
+
+x=45
+if x>50:
+    print('over 50')
+else: print('les than 50')       
+
+x=0
+while x<10:
+    x+=1
+    print(x)
+
+x=[1,2,3,4,5]
+for i in x: 
+    i=i+1  
+    print(i)
+
+def mltpy(w,h):
+    v=w*h
+    return v
+
+p=mltpy(8,9.85)
+print(p)
+
+
+A=[1, 4, 5]
+x=75;
+y=65;
+for i in A:
+    i=i+x+y; 
+    print(i)
+
+if x>y:
+    z=x+y
+    print(z)
+else:
+    z=x-y
+    print(z)
+
+
+"""
+x=81
+y=math.sqrt(x)
+print(y)
+
+
+
+ 
+
